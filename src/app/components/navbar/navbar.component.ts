0aae707b6df26c0efdaa88805d410f60e1eb0750
Endriu <endriu.abazi@greenleaf-bsc.com>
2023-05-29 21:19:49 +0200

    Table is resposive plus better designs

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,24 +1,38 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss'],
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-  emri: any = localStorage.getItem('name');
-  role: any = localStorage.getItem('role');
+    emri: any = localStorage.getItem('name');
+    role: any = localStorage.getItem('role');
+    greeting?: string;
+    constructor(private router: Router) {}
+    ngOnInit() {
+        this.getTimeOfDay();
+    }
 
-  constructor(private router: Router) {}
+    logout() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('role');
+        this.router.navigate(['']);
+    }
 
-  logout() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('role');
-    this.router.navigate(['']);
-  }
+    isHrRole(): boolean {
+        return this.role === 'HR'; // Adjust the condition based on your role values
+    }
 
-  isHrRole(): boolean {
-    return this.role === 'HR'; // Adjust the condition based on your role values
-  }
+    getTimeOfDay() {
+        const hour = new Date().getHours();
+        if (hour >= 5 && hour < 12) {
+            this.greeting = 'Mirëmëngjes';
+        } else if (hour >= 12 && hour < 16) {
+            this.greeting = 'Mirëdita';
+        } else {
+            this.greeting = 'Mirëmbrema';
+        }
+    }
 }
